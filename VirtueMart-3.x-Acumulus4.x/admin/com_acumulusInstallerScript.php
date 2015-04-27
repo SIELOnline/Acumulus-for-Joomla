8e491eedaafd2eba2bb16e61b36a1e8cdad8ece7
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2015-04-27 13:01:36 +0200

    Testing installation.

--- VirtueMart-3.x-Acumulus4.x/admin/com_acumulusInstallerScript.php
+++ VirtueMart-3.x-Acumulus4.x/admin/com_acumulusInstallerScript.php
@@ -7,7 +7,7 @@ use Siel\Acumulus\Helpers\Requirements;
 /**
  * Script file of HelloWorld module
  */
-class mod_acumulusInstallerScript {
+class com_acumulusInstallerScript {
 
   /**
    * Method to install the extension
@@ -15,8 +15,10 @@ class mod_acumulusInstallerScript {
    *
    * @return void
    */
-  function install($parent) {
-    echo '<p>The Acumulus component has been installed. Please visit the settings form.</p>';
+  public function install($parent) {
+    $version = (string) $parent->get('manifest')->version;
+    JFactory::getApplication()->enqueueMessage("The Acumulus component ($version) has been installed. Please fill in the settings form.", 'message');
+    $parent->getParent()->setRedirectURL('index.php?option=com_acumulus&task=config');
   }
 
   /**
@@ -25,46 +27,89 @@ class mod_acumulusInstallerScript {
    *
    * @return void
    */
-  function uninstall($parent) {
-    echo '<p>The Acumulus component has been uninstalled</p>';
+  public function uninstall(/*$parent*/) {
+    JFactory::getApplication()->enqueueMessage('The Acumulus component has been uninstalled.', 'message');
   }
 
   /**
    * Method to update the extension
    * $parent is the class calling this method
    *
-   * @return void
+   * @param object $parent
+   *
+   * @throws \Exception
    */
-  function update($parent) {
-    echo '<p>The Acumulus component has been updated to version' . $parent->get('manifest')->version . '.</p>';
+  public function update($parent) {
+    $version = (string) $parent->get('manifest')->version;
+    JFactory::getApplication()->enqueueMessage("The Acumulus component has been updated to version $version.", 'message');
   }
 
   /**
-   * Method to run before an install/update/uninstall method
-   * $parent is the class calling this method
-   * $type is the type of change (install, update or discover_install)
+   * Method to run before an install/update/uninstall method.
+   *
+   * This method checks:
+   * - The Joomla version against the version attribute in the manifest.
+   * - If this is not a downgrade of this module.
+   * - If VirtueMart is installed.
+   * - The Virtuemart against the minVirtueMartVersion element in the manifest.
+   * - The dependencies on PHP extensions of this module.
+   *
+   * @param string $type
+   *   The type of change (install, update or discover_install)
+   * @param JInstallerAdapter $parent
+   *   The object calling this method.
    *
    * @return bool
    */
-  function preflight($type, $parent) {
-    if (in_array($type, array('install', 'update'))) {
-      // Check if VirtueMart is installed.
-      jimport('joomla.application.component.controller');
-      if (!JComponentHelper::isEnabled('com_virtuemart')) {
-        $installer = JInstaller::getInstance();
-        $installer->abort('The Acumulus component requires VirtueMart to be installed and enabled.');
-        return FALSE;
-      }
+  public function preflight($type, $parent) {
+    $version = (string) $parent->get("manifest")->version;
 
-      // Get access to our classes via the auto loader.
-      JLoader::registerNamespace('Siel', dirname(__FILE__) . '/libraries');
-      $errors = Requirements::check();
-      if (empty($errors)) {
-        $installer = JInstaller::getInstance();
-        $installer->abort(implode(' ', $errors));
+    // Check Joomla version: @todo check what version will fail.
+    $minJoomlaVersion = $parent->get("manifest")->attributes()->version;
+    if (version_compare((new JVersion())->getShortVersion(), $minJoomlaVersion, 'lt')) {
+      JInstaller::getInstance()->abort("The Acumulus component ($version) requires at least Joomla $minJoomlaVersion.");
+      return FALSE;
+    }
+
+    // Check downgrade.
+    if ($type == 'update') {
+      $currentInfo = json_decode($parent->get('extension')->manifest_cache, TRUE);
+      $currentRelease = $currentInfo['version'];
+      if (version_compare($version, $currentRelease, 'lt')) {
+        JInstaller::getInstance()->abort("The Acumulus component ($currentRelease) cannot be downgraded to $version.");
         return FALSE;
       }
     }
+
+    // Check if VirtueMart is installed.
+    jimport('joomla.application.component.controller');
+    if (!JComponentHelper::isEnabled('com_virtuemart')) {
+      JInstaller::getInstance()->abort("'The Acumulus component $version requires VirtueMart to be installed and enabled.");
+      return FALSE;
+    }
+
+    // Check Virtuemart version: @todo: determine which version will fail.
+    /** @var JTableExtension $extension */
+    $extension = JTable::getInstance('extension');
+    $id = $extension->find(array('element' => 'com_virtuemart'));
+    $extension->load($id);
+    $componentInfo = json_decode($extension->manifest_cache, TRUE);
+    $shopVersion = $componentInfo['version'];
+    $minVirtueMartVersion = (string) $parent->get("manifest")->minVirtueMartVersion;
+    if (version_compare($shopVersion, $minVirtueMartVersion, 'lt')) {
+      JInstaller::getInstance()->abort("The Acumulus component $version requires at least VirtueMart $minVirtueMartVersion.");
+      return FALSE;
+    }
+
+    // Check extension requirements.
+    // Get access to our classes via the auto loader.
+    JLoader::registerNamespace('Siel', dirname(__FILE__) . '/admin/libraries');
+    $errors = Requirements::check();
+    if (!empty($errors)) {
+      JInstaller::getInstance()->abort(implode(' ', $errors));
+      return FALSE;
+    }
+
     return TRUE;
   }
 
@@ -73,8 +118,9 @@ class mod_acumulusInstallerScript {
    * $parent is the class calling this method
    * $type is the type of change (install, update or discover_install)
    *
-   * @return void
+   * @param string $type
+   * @param object $parent
    */
-  function postflight($type, $parent) {
+  public function postflight($type, $parent) {
   }
 }
---
+++ zip.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\7-Zip\7z.exe" a -tzip VirtueMart-3.x-Acumulus4.x.zip VirtueMart-3.x-Acumulus4.x
+"C:\Program Files\7-Zip\7z.exe" t VirtueMart-3.x-Acumulus4.x.zip
+
