0eda649877d5553c420447f9df99fbc8ac583daf
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-10-19 12:48:28 +0200

    Allow for entry id being null and thus also for multiple results on getByEntryId

--- acumulus/com_acumulus/admin/tables/acumulusentry.php
+++ acumulus/com_acumulus/admin/tables/acumulusentry.php
@@ -40,4 +40,58 @@ class AcumulusTableAcumulusEntry extends JTable
     {
         parent::__construct('#__acumulus_entry', 'id', $db ? $db : JFactory::getDbo());
     }
+
+    /**
+     * Returns a set of instances that satisfy the filters.
+     *
+     * @param array $keys
+     *   The filters to query on as a set of field => value pairs.
+     *
+     * @return AcumulusTableAcumulusEntry[]
+     *   The, possibly empty, result set.
+     */
+    public function loadMultiple(array $keys) {
+        $reset = true;
+
+        if ($reset)
+        {
+            $this->reset();
+        }
+
+        // Initialise the query.
+        $query = $this->_db->getQuery(true)->select('*')->from($this->_tbl);
+        $fields = array_keys($this->getProperties());
+
+        foreach ($keys as $field => $value)
+        {
+            // Check that $field is in the table.
+            if (!in_array($field, $fields))
+            {
+                throw new UnexpectedValueException(sprintf('Missing field in database: %s &#160; %s.', get_class($this), $field));
+            }
+            // Add the search tuple to the query.
+            $operator = $value === null ? 'is' : '=';
+            $value = $value === null ? 'null' : $this->_db->quote($value);
+            $query->where($this->_db->quoteName($field) . $operator . $value);
+        }
+
+        $this->_db->setQuery($query);
+
+        $isFirst = true;
+        $result = array();
+        $rows = $this->_db->loadAssocList();
+        if ($rows !== null) {
+            foreach ($rows as $row) {
+                if ($isFirst) {
+                    $this->bind($row);
+                    $result[] = $this;
+                    $isFirst = false;
+                }
+                else {
+                    $result[] = (new static())->bind($row);
+                }
+            }
+        }
+        return $result;
+    }
 }
