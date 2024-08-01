5b25a05c58b0b92571330d485642459e93038ce1
Erwin Derksen <erwin@burorader.com>
2024-08-01 21:28:31 +0200

    Joomla package now supports Joomla 5 (continuing support for J4 to be tested)

---
+++ com_acumulus/admin/tests/app.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @noinspection PhpMissingStrictTypesDeclarationInspection
+ * @noinspection DuplicatedCode Proudly copied (and slightly adapted) from
+ *   /administrator/ includes/app.php.
+ */
+
+use Joomla\CMS\Application\AdministratorApplication;
+use Joomla\CMS\Factory;
+use Joomla\DI\Container;
+
+\defined('_JEXEC') or die;
+
+class AppLoader
+{
+    /**
+     * @noinspection UntrustedInclusionInspection
+     */
+    public function execute(string $administratorPath): Container
+    {
+        if (file_exists("$administratorPath/defines.php")) {
+            require_once "$administratorPath/defines.php";
+        }
+
+        require_once "$administratorPath/includes/defines.php";
+
+        // Check for presence of vendor dependencies not included in the git repository
+        if (!file_exists(JPATH_LIBRARIES . '/vendor/autoload.php') || !is_dir(JPATH_PUBLIC . '/media/vendor')) {
+            echo file_get_contents(JPATH_ROOT . '/templates/system/build_incomplete.html');
+            exit;
+        }
+
+        require_once JPATH_BASE . '/includes/framework.php';
+
+        // Boot the DI container
+        $container = Factory::getContainer();
+
+        /*
+         * Alias the session service keys to the web session service as that is the primary session backend for this application
+         *
+         * In addition to aliasing "common" service keys, we also create aliases for the PHP classes to ensure autowiring objects
+         * is supported.  This includes aliases for aliased class names, and the keys for aliased class names should be considered
+         * deprecated to be removed when the class name alias is removed as well.
+         */
+        $container->alias('session.web', 'session.web.administrator')
+            ->alias('session', 'session.web.administrator')
+            ->alias('JSession', 'session.web.administrator')
+            ->alias(\Joomla\CMS\Session\Session::class, 'session.web.administrator')
+            ->alias(\Joomla\Session\Session::class, 'session.web.administrator')
+            ->alias(\Joomla\Session\SessionInterface::class, 'session.web.administrator');
+
+        // Instantiate the application.
+        $app = $container->get(AdministratorApplication::class);
+
+        // Set the application as global app
+        /** @noinspection DisallowWritingIntoStaticPropertiesInspection */
+        Factory::$application = $app;
+
+        return $container;
+    }
+}
--- com_acumulus/admin/tests/bootstrap-acumulus-J5.php
+++ com_acumulus/admin/tests/bootstrap-acumulus-J5.php
@@ -19,10 +19,16 @@
  *   Config. To get it right in our component requires to set it in the Application.
  *
  * @noinspection PhpIllegalPsrClassPathInspection  Bootstrap file is loaded directly.
+ * @noinspection DuplicatedCode  A duplicate for J4 exists.
+ * @noinspection PhpMultipleClassDeclarationsInspection  A duplicate for J4 exists.
  */
 
 declare(strict_types=1);
 
+/**
+ * Constant that is checked in included files to prevent direct access.
+ * define() is used rather than "const" to not error for PHP 5.2 and lower
+ */
 const _JEXEC = 1;
 
 /**
@@ -33,9 +39,6 @@ const _JEXEC = 1;
 use Joomla\CMS\Application\AdministratorApplication;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\LanguageFactoryInterface;
-use Joomla\CMS\MVC\Factory\LegacyFactory;
-use Joomla\Session\Session;
-use Joomla\Session\SessionInterface;
 
 /**
  * Class AcumulusTestsBootstrap bootstraps the Acumulus tests.
@@ -50,7 +53,6 @@ use Joomla\Session\SessionInterface;
 class AcumulusTestsBootstrap
 {
     private static AcumulusTestsBootstrap $instance;
-    private LegacyFactory $factory;
     private AcumulusModelAcumulus $model;
 
     /**
@@ -107,38 +109,17 @@ class AcumulusTestsBootstrap
         if (!isset($_SERVER['SCRIPT_NAME'])) {
             $_SERVER['SCRIPT_NAME'] = '/administrator/index.php';
         }
+        if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+            $_SERVER['HTTP_USER_AGENT'] = 'Firefox';
+        }
 
         $administratorPath = $this->getAdministratorPath();
-        // From app.php
+        // Load and execute app.php
         if (!defined('JPATH_BASE')) {
             define('JPATH_BASE', $administratorPath);
         }
-        require_once $administratorPath . '/includes/defines.php';
-
-        // Check for presence of vendor dependencies not included in the git repository
-        if (!file_exists(JPATH_LIBRARIES . '/vendor/autoload.php') || !is_dir(JPATH_ROOT . '/media/vendor')) {
-            echo file_get_contents(JPATH_ROOT . '/templates/system/build_incomplete.html');
-            exit;
-        }
-
-        require_once JPATH_BASE . '/includes/framework.php';
-
-        // Boot the DI container
-        $container = Factory::getContainer();
-
-        /*
-         * Alias the session service keys to the web session service as that is the primary session backend for this application
-         *
-         * In addition to aliasing "common" service keys, we also create aliases for the PHP classes to ensure autowiring objects
-         * is supported.  This includes aliases for aliased class names, and the keys for aliased class names should be considered
-         * deprecated to be removed when the class name alias is removed as well.
-         */
-        $container->alias('session.web', 'session.web.administrator')
-            ->alias('session', 'session.web.administrator')
-            ->alias('JSession', 'session.web.administrator')
-            ->alias(\Joomla\CMS\Session\Session::class, 'session.web.administrator')
-            ->alias(Session::class, 'session.web.administrator')
-            ->alias(SessionInterface::class, 'session.web.administrator');
+        include_once __DIR__ . '/app.php';
+        $container = (new AppLoader())->execute($administratorPath);
 
         // Ensure we (and other code) use the nl-NL language.
         /** @var Joomla\Registry\Registry $config */
@@ -148,29 +129,14 @@ class AcumulusTestsBootstrap
         $locale = 'nl-NL';
         $config->set('language', $locale);
         $language = $languageFactory->createLanguage($locale);
-
-        // Instantiate the application.
         /** @var AdministratorApplication $app */
         $app = $container->get(AdministratorApplication::class);
         $app->loadLanguage($language);
-        // And set it as global app.
-        /** @noinspection DisallowWritingIntoStaticPropertiesInspection */
-        Factory::$application = $app;
-
-        // Eventually, we should move to the MVCFactory, but that probably only works in
-        // J4. Restricting our tests just to J4 is not a problem, but we probably have to
-        // restructure "all" our MVC classes (rename, use namespaces, ...) and that would
-        // make our component run only on J4, and it is too early for that (sep. 2023).
-        $this->factory = new LegacyFactory();
-
-        // Start the session before the ConsoleApp starts outputting "headers" and we get
-        // an error "Failed to start the session because headers have already been sent
-        // by ..." from {@see// Joomla\Session\Storage\NativeStorage::start()}
-        $app->getSession()->get('user');
 
         // This line prevents a "Class 'Joomla\Component\Templates\Administrator\Extension\TemplatesComponent'
         // not found" error on HS4 + J4.2
         JLoader::registerNamespace('Joomla\Component\Templates\Administrator', JPATH_ADMINISTRATOR . '/components/com_templates/src');
+
         $this->loadCompatBehaviour();
         $this->loadAcumulus();
     }
@@ -186,12 +152,61 @@ class AcumulusTestsBootstrap
         $administratorPath = $this->getAdministratorPath();
         require_once $administratorPath . '/components/com_acumulus/models/acumulus.php';
         /** @noinspection PhpFieldAssignmentTypeMismatchInspection */
-        $this->model = $this->factory->createModel('Acumulus', 'AcumulusModel', ['name' => 'Acumulus']);
+        $this->model = Factory::getContainer()->buildObject('AcumulusModelAcumulus');
     }
 
     private function loadCompatBehaviour(): void
     {
-        Joomla\CMS\Plugin\PluginHelper::getPlugin('behaviour', 'compat');
+        /** @noinspection ClassConstantCanBeUsedInspection */
+        $aliases = [
+            'JObject' => 'Joomla\CMS\Object\CMSObject',
+            'JTable' => 'Joomla\CMS\Table\Table',
+            'JComponentHelper' => 'Joomla\CMS\Component\ComponentHelper',
+            'JApplicationHelper' => 'Joomla\CMS\Application\ApplicationHelper',
+            'JTableContenttype' => 'Joomla\CMS\Table\ContentType',
+            'JFactory' => 'Joomla\CMS\Factory',
+            'JInstaller' => 'Joomla\CMS\Installer\Installer',
+            'JControllerLegacy' => 'Joomla\CMS\MVC\Controller\BaseController',
+            'JViewLegacy' => 'Joomla\CMS\MVC\View\HtmlView',
+            'JRoute' => 'Joomla\CMS\Router\Route',
+            'JURI' => 'Joomla\CMS\Uri\Uri',
+            'JPlugin' => 'Joomla\CMS\Plugin\CMSPlugin',
+            'JPluginHelper' => 'Joomla\CMS\Plugin\PluginHelper',
+            'JModuleHelper' => 'Joomla\CMS\Helper\ModuleHelper',
+            'JRegistry' => 'Joomla\Registry\Registry',
+            'JFilterInput' => 'Joomla\CMS\Filter\InputFilter',
+            'JFilterOutput' => 'Joomla\CMS\Filter\OutputFilter',
+            'JLanguage' => 'Joomla\CMS\Language\Language',
+            'JLanguageHelper' => 'Joomla\CMS\Language\LanguageHelper',
+            'JLanguageAssociations' => 'Joomla\CMS\Language\Associations',
+            'JText' => 'Joomla\CMS\Language\Text',
+            'JFile' => 'Joomla\CMS\Filesystem\File',
+            'JFolder' => 'Joomla\CMS\Filesystem\Folder',
+            'JPath' => 'Joomla\CMS\Filesystem\Path',
+            'JMailHelper' => 'Joomla\CMS\Mail\MailHelper',
+            'JUserHelper' => 'Joomla\CMS\User\UserHelper',
+            'JUser' => 'Joomla\CMS\User\User',
+            'JAccess' => 'Joomla\CMS\Access\Access',
+            'JSession' => 'Joomla\CMS\Session\Session',
+            'JButton' => 'Joomla\CMS\Toolbar\ToolbarButton',
+            'JToolbarHelper' => 'Joomla\CMS\Toolbar\ToolbarHelper',
+            'JToolbar' => 'Joomla\CMS\Toolbar\Toolbar',
+            'JPagination' => 'Joomla\CMS\Pagination\Pagination',
+            'JLayoutFile' => 'Joomla\CMS\Layout\FileLayout',
+            'JHTML' => 'Joomla\CMS\HTML\HTMLHelper',
+            'JHTMLSelect' => 'Joomla\CMS\HTML\Helpers\Select',
+            'JEditor' => 'Joomla\CMS\Editor\Editor',
+            'JForm' => 'Joomla\CMS\Form\Form',
+            'JFormHelper' => 'Joomla\CMS\Form\FormHelper',
+            'JFormField' => 'Joomla\CMS\Form\FormField',
+            'JUtility' => 'Joomla\CMS\Utility\Utility',
+            'JCache' => 'Joomla\CMS\Cache\Cache',
+        ];
+        foreach ($aliases as $alias => $original) {
+            if (!class_exists($alias)) {
+                class_alias($original, $alias);
+            }
+        }
     }
 }
 
