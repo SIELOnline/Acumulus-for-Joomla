ebca1adadb503a46f95b1b9cfb3fb9d23f381595
Erwin Derksen <erwin@burorader.com>
2024-11-28 10:05:37 +0100

    Joomla (VirtueMart and HikaShop) test data update: keys to lower case

--- com_acumulus/admin/tests/Joomla/AcumulusTestUtils.php
+++ com_acumulus/admin/tests/Joomla/AcumulusTestUtils.php
@@ -18,6 +18,7 @@ trait AcumulusTestUtils
 {
     use BaseAcumulusTestUtils {
         copyLatestTestSources as protected parentCopyLatestTestSources;
+        updateTestSources as protected parentUpdateTestSources;
     }
 
     protected static function getAcumulusComponent(): AcumulusComponent
@@ -46,5 +47,16 @@ trait AcumulusTestUtils
             require_once dirname(__FILE__, 2) . '/bootstrap-acumulus.php';
         }
         $this->parentCopyLatestTestSources();
+    }
+
+    public function updateTestSources(): void
+    {
+        static $hasRun = false;
+
+        if (!$hasRun) {
+            $hasRun = true;
+            require_once dirname(__FILE__, 2) . '/bootstrap-acumulus.php';
+        }
+        $this->parentUpdateTestSources();
     }
 }
